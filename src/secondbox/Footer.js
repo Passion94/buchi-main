9f9138abc8db43304ffe2fb1c6f6b3627c05b118
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-11-08 07:40:44 -0800

    Completetd project with Footer icons

--- src/secondbox/Footer.js
+++ src/secondbox/Footer.js
@@ -1,5 +1,9 @@
 
 import FacebookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
+import StackedLineChartOutlinedIcon from '@mui/icons-material/StackedLineChartOutlined';
+import NotListedLocationOutlinedIcon from '@mui/icons-material/NotListedLocationOutlined';
+import FlutterDashOutlinedIcon from '@mui/icons-material/FlutterDashOutlined';
+
 
 const date= new Date().getFullYear();
 
@@ -8,10 +12,11 @@ function Footer() {
     <div className="footer">
       <p className="para"> Find me on social media.</p>
       
-       <a className="links" href="https://www.quora.com/profile/Peter-Moses-Mohemos">Q</a>
-      <a className="links" href="https://stackoverflow.com/users/5482255/peter-moses"></a>
-      <a className="links" href="https://web.facebook.com/mohemos"><FacebookTwoToneIcon className="facebook"/></a>
-      <a className="links" href="https://twitter.com/mohemos"></a>
+       <a className="links" href="https://www.quora.com/profile/Peter-Moses-Mohemos" target="_blank"><i class="fa fa-quora w3-hover-opacity"></i></a>
+       <a href="https://stackoverflow.com/users/5482255/peter-moses-mohemos" target="_blank"><i class="fa fa-stack-overflow w3-hover-opacity"></i></a>
+       <a className="links" href="https://web.facebook.com/mohemos" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+       <a className="links" href="https://twitter.com/mohemos" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+       
       <p className="para para2"> © Moses Hennuho Peter, {date}</p>
     </div>
   );
